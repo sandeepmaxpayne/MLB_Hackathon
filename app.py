ec277371293b9514d9eea2ac801c61ba90beea98
SANDEEP DUTTA <sandeepduttacse45@gmail.com>
2025-02-03 12:34:46 +0530

    updated docker file

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import time
 from flask import Flask, jsonify, render_template, request
-
+from dotenv import load_dotenv
+import os
 from mlb_tip_generator import generate_tagline
+from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+
+
+# Get environment variables
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
 
 @app.route('/')
 def index():
